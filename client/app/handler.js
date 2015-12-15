d967fc72883d2b339f83805ee9ca35615cec5dd0
eric <q465168867@gmail.com>
2015-12-15 22:29:54 +0800

    Under construction

--- client/app/handler.js
+++ client/app/handler.js
@@ -9,8 +9,15 @@ app.controller('main',['$scope','$sce',handler]);
 
 function handler($scope,$sce){
   $scope.isLogin=false;
+  $scope.isRoot=false;
   $scope.error=false;
-  $scope.token="";
+  $scope.cmd={
+    opengame:'',
+    kickout:'',
+    token:'',
+    closegame:''
+  }
+  $scope.userList=[]
   $scope.user={
     username:'',
     addr:'',
@@ -21,28 +28,9 @@ function handler($scope,$sce){
   $scope.game=game;
   $scope.game.start();
   $scope.board=$scope.game.getBoard();
-  // $scope.board=[
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0],
-  //   [0,0,0,0,0,0,0,0]
-  // ];
-  // for(var i=0;i<8;i++){
-  //   for(var j=0;j<8;j++){
-  //     $scope.board[i][j]={isBlack:false,isWhite:false};
-  //   }
-  // }
-  // $scope.board[3][3].isBlack=true;
-  // $scope.board[4][4].isBlack=true;
-  // $scope.board[3][4].isWhite=true;
-  // $scope.board[4][3].isWhite=true;
   var send=function(msg){
     if($scope.isLogin){
-      ipc.send('msg',msg+$scope.token);
+      ipc.send('msg',[msg,$scope.token].join(" "));
     }else{
       ipc.send('msg',msg);
     }
@@ -59,6 +47,39 @@ function handler($scope,$sce){
         send("LOGIN "+$scope.user.username);
       }
   };
+
+  $scope.openGame=function(){
+    if($scope.cmd.opengame!=""){
+      send("OPENGAME "+$scope.cmd.opengame);
+    }
+  };
+
+  $scope.list=function(){
+    send("LIST");
+  };
+
+  $scope.kickOut=function(){
+    if($scope.cmd.kickout!=''){
+      send(["KICKOUT",$scope.cmd.kickout].join(" "));
+    }
+  };
+
+  $scope.games=function(){
+      send("GAMES");
+  };
+
+  $scope.watch=function(){
+    if($scope.isRoot){
+      send(["WATCH",$scope.cmd.watch].join(" "));
+    }
+  };
+
+  $scope.closegame=function(){
+    if($scope.isRoot){
+      send(["CLOSEGAME",$scope.cmd.closegame].join(" "));
+    }
+  };
+
   $scope.move=function(x,y){
     point=$scope.board[x][y]
     if((!point.isBlack)&&(!point.isWhite)){
@@ -68,18 +89,38 @@ function handler($scope,$sce){
     }
   };
   ipc.on('msg',function(event,message){
-    cmd=message.toString().split(" ");
+    cmd=message.toString().trim().split(" ");
     console.log(cmd)
     switch (cmd[0]) {
+      case "PING":
+        send("PONG")
+        break;
+      case "ROOT":
+        $scope.token=cmd[2];
+        $scope.isLogin=true;
+        $scope.isRoot=true;
+        $scope.list();
+        $scope.$apply();
+        break;
       case "LOGIN":
         if(cmd[1]=="SUCCESS"){
           $scope.token=cmd[2];
           $scope.isLogin=true;
+          $scope.list();
           $scope.$apply();
         }
         else{
             alert("水能载舟，亦可赛艇");
         }
+        break;
+      case "LIST":
+        temp=[];
+        for(var i=1;i<cmd.length;i=i+2){
+          temp.push({username:cmd[i],free:(cmd[i+1]=="free"),status:cmd[i+1]});
+        }
+        $scope.userList=temp;
+        // // console.log("LIST")
+        // console.log("LIST:"+$scope.userList);
         break;
       // default:
     }
